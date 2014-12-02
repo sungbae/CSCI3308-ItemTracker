81da15316fc184dc538cb229b23ebd60a8ee7c03
sung <sungbae305@gmail.com>
2014-12-01 21:53:06 -0700

    remove personal information

--- html/search/connectOld.php
+++ html/search/connectOld.php
@@ -1,7 +1,6 @@
 <?php
  // Connects to Our Database
-// $link = new mysqli('localhost', 'username', 'password', 'db_name');
-$link = new mysqli('192.168.1.112', 'suba8204', 'encore305', 'csci3308_project');
+$link = new mysqli('localhost', 'username', 'password', 'db_name');
 
 /* check connection */
 if (mysqli_connect_errno()) {
