7babdeb099197ea36e49ab75bf0f295a8dc0633e
bebu8997 <bebu8997@colorado.edu>
2014-12-08 19:45:53 -0700

    Trying new php query approach

--- html/search/ben_search/searchItems.php
+++ html/search/ben_search/searchItems.php
@@ -3,8 +3,8 @@ include 'connect.php';
 
 $search = $_POST['search']."*";
 
-$search_query = $link->prepare("SELECT * FROM `itemTracker` WHERE Item = ?");
-$search_query->bind_param('s', $search);
+$search_query = $link->prepare("SELECT * FROM `itemTracker` WHERE Item = $search");
+//$search_query->bind_param('s', $search);
 $search_query->execute();
 $search_query->store_result();
 $search_rows = $search_query->num_rows;
