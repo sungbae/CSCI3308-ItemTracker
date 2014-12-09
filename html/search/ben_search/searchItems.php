db7b6cbb4d136bf78a40a29b848ecaeb12a6a708
yiyangqi <yiyang515@yahoo.com>
2014-12-08 21:06:34 -0700

    new changes

--- html/search/ben_search/searchItems.php
+++ html/search/ben_search/searchItems.php
@@ -4,17 +4,20 @@ include 'connect.php';
 $name = $_POST["name"]."*";
 var_dump($_POST["name"]);
 
-$search_query = $link->prepare("SELECT * FROM `itemTracker` WHERE Item = (?)");
+$search_query = $link->prepare("SELECT * FROM `itemTracker` WHERE Item = ?");
 $search_query->bind_param('s', $name);
 $search_query->execute();
 $search_query->store_result();
 $search_rows = $search_query->num_rows;
-$search_query->bind_result($product_name);
+$search_query->bind_result($product_name, $person_name);
 
-if($search_rows > 0){
+echo "$search_rows";
+
+if($search_rows >= 0){
+
 while($search_query->fetch()){
 echo "Your search returned $search_rows results";
-echo "$product_name <br>";
+echo "$product_name is with $person_name <br>";
 }
 } else { echo "Your search did not return any results, please try again."; }
  ?>
