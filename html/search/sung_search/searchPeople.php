6ddb46b473ad91551a0ed8b6dd18e582ccaea52e
yiyangqi <yiyang515@yahoo.com>
2014-12-08 19:42:13 -0700

    update on search php

--- html/search/sung_search/searchPeople.php
+++ html/search/sung_search/searchPeople.php
@@ -7,17 +7,17 @@ include 'connect.php';
 
 $search = $_POST['search']."*";
 
-$search_query = $link->prepare("SELECT items FROM item WHERE MATCH(name) AGAINST (? IN BOOLEAN MODE)")
+$search_query = $link->prepare("SELECT * FROM `itemTracker` WHERE Name = ?")
 $search_query->bind_param('s', $search);
 $search_query->execute();
 $search_query->store_result();
 $search_rows = $search_query->num_rows;
-$search_query->bind_result($product_name);
+//$search_query->bind_result($product_name);
 
 if($search_rows > 0){
 while($search_query->fetch()){
 echo "$search has borrowed $search_rows item(s):";
-echo "$product_name <br>";
+//echo "$product_name <br>";
 }
 } else { echo "Your search did not return any results, please try again."; }
 ?>
